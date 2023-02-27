3c3d93b213a777a8c4f3d5d6a4da2a54b9095554
ernarl <arl.ernestas@gmail.com>
2023-02-27 19:21:49 +0200

    Voluteer movement to storage and trash pick up points

--- Assets/Scripts/Volunteer.cs
+++ Assets/Scripts/Volunteer.cs
@@ -5,15 +5,45 @@ using UnityEngine.AI;
 
 public class Volunteer : MonoBehaviour
 {
-    public NavMeshAgent navMeshAgent;
+    [SerializeField] private NavMeshAgent navMeshAgent;
 
+    [Header("Movement Settings")]
+    [SerializeField] private GameObject[] movementPoints;
+    [SerializeField] private GameObject storage;
+    [SerializeField] private float timeBetweenMoves = 5f;
+    private void Awake()
+    {
+    }
     void Start()
     {
-        navMeshAgent.SetDestination(new Vector3(-3f,0f,-3));
+        StartCoroutine(PickUpAfter(timeBetweenMoves));
     }
 
     public void MoveTo(Vector3 moveLocation)
     {
         navMeshAgent.SetDestination(moveLocation);
     }
+    public void GoToPickUpLocation()
+    {
+        int randomInt = Random.Range(0, movementPoints.Length);
+        MoveTo(movementPoints[randomInt].transform.position);
+    }
+    public void GoToPutDownLocation()
+    {
+        MoveTo(storage.transform.position);
+    }
+
+    IEnumerator PickUpAfter(float time)
+    {      
+        GoToPickUpLocation();
+        yield return new WaitForSeconds(time);
+        StartCoroutine(PutDownAfter(time));
+    }
+    IEnumerator PutDownAfter(float time)
+    {       
+        GoToPutDownLocation();
+        yield return new WaitForSeconds(time);
+        StartCoroutine(PickUpAfter(time));
+    }
+
 }
