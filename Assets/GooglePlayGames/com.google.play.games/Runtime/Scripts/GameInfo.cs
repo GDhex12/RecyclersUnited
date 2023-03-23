b0fb4905e6197986f8d4e8e19fa69a40fdc3bda7
Benas Dorochinas <benassdo@gmail.com>
2023-03-23 22:21:56 +0200

    GooglePlayGames: service init

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "700426651559"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "700426651559-3u3o1pslpcfgkk0ctv3guu3olmu3hmj2.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
