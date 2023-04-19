fd3a3127bc7f3c70b1bfb04704835ec7751c0f5e
Arvydas <arvydas.mazauskas@ktu.edu>
2023-04-19 16:55:56 +0300

    Renewed vehicle prefab

--- Assets/Scripts/Debug/VehicleCooldown.cs
+++ Assets/Scripts/Debug/VehicleCooldown.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class VehicleCooldown : MonoBehaviour
 {
-    public int timeoutSeconds;
+    public float timeoutSeconds;
     public TMP_Text timeString;
     public GameObject cooldownContainer;
     public VehicleSystem vehicleManager;
@@ -15,6 +15,7 @@ public class VehicleCooldown : MonoBehaviour
     //visual update
     [SerializeField] Image cooldownImage;
     [SerializeField] GameObject cooldownUI_container;
+    [SerializeField] TextMeshProUGUI currency;
 
     public bool vehicleReturned = true;
 
@@ -29,18 +30,21 @@ public class VehicleCooldown : MonoBehaviour
     {
         vehicleReturned = true;
         cooldownUI_container.SetActive(false);
-        GetComponent<Animator>().Play(string.Format("{0}Returns", gameObject.name));
+        //GetComponent<Animator>().Play(string.Format("{0}Returns", gameObject.name));
+        GetComponent<Animator>().Play("TruckReturns");
     }
 
     private IEnumerator CountdownLoop ()
     {
         vehicleReturned = false;
         lastSentCount = vehicleManager.currentGarbageCount;
+        currency.text = vehicleManager.GetGarbageToMoneyToString(lastSentCount);
         vehicleManager.RemoveAllGarbage();
-        for (int i=timeoutSeconds; i>=0; i--)
+        for (int i=(int)timeoutSeconds; i>=0; i--)
         {
             //timeString.text = string.Format("{0} s", i);
-            cooldownImage.fillAmount = (float)(i / timeoutSeconds);
+            //cooldownImage.fillAmount = (float)((timeoutSeconds - i) / timeoutSeconds); //filling 
+            cooldownImage.fillAmount = (float)(i / timeoutSeconds); //emptying
             yield return new WaitForSeconds(1f);
         }
         TimerShutdown();
