ecaa0d29ce10256a74dd97cf630a9d4c6c58ba1c
Arvydas <arvydas.mazauskas@ktu.edu>
2023-05-08 17:44:12 +0300

    AppStartup code cleanup and vehicle fill off scene

--- Assets/Scripts/AppStartup.cs
+++ Assets/Scripts/AppStartup.cs
@@ -3,13 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class AppStartup : MonoBehaviour
 {
+    [Header("General")]
     const int bootSceneNo = 0;
     public static bool veryFirstCallInApp = true;
     [SerializeField] private TimeManager refToTime;
-    [SerializeField] private Storage refToStorage;
+
+    [Header("Storage")]
+    Storage refToStorage;
+    [SerializeField] float storage_timeBtwAdditions = 5;
+
+    [Header("Vehicle")]
+    VehicleSystem refToVehicle;
+    [SerializeField] float vehicle_timeBtwAdditions = 4;
 
     void Start()
     {
+        refToStorage = GameManager.Instance.storage;
+        refToVehicle = GameManager.Instance.vehicle;
+
         if (veryFirstCallInApp)
         {
             ProgramBegins();
@@ -25,12 +36,35 @@ public class AppStartup : MonoBehaviour
     void ProgramBegins()
     {
         float timeDifference = TimeManager.instance.TimeDiffBtwExitAndStart();
-        int storageDifference = (int)timeDifference / 3;
-        int storageLevelFormula = 50 + (PersistantData.Instance.sceneData.StorageCapacityCurrentLevel * 25);
-        if (PersistantData.Instance.sceneData.StorageGarbageCount + storageDifference > storageLevelFormula)
+        AddStorageGarbage(timeDifference);
+        AddVehicleGarbage(timeDifference);
+    }
+
+    void AddStorageGarbage(float timeDifference)
+    {
+        int volunteerCount = PersistantData.Instance.sceneData.VolunteerPickerCount;
+        int storageDifference = (int)(timeDifference / storage_timeBtwAdditions) * volunteerCount;
+
+        // ------------maksimumas jau yra paskaiciuotas kitur---------------------
+        //int storageLevelFormula = 50 + (PersistantData.Instance.sceneData.StorageCapacityCurrentLevel * 25);
+
+        int maxStorage = refToStorage.GetMaxGarbageCount(); // asikesnis vardas
+
+        if (PersistantData.Instance.sceneData.StorageGarbageCount + storageDifference > maxStorage)
         {
-            refToStorage.AddGarbage(refToStorage.GetMaxGarbageCount() - refToStorage.GetGarbageCount());
+            storageDifference = refToStorage.GetMaxGarbageCount() - refToStorage.GetGarbageCount();
         }
-        else refToStorage.AddGarbage(storageDifference);
+
+
+        refToStorage.AddGarbage(storageDifference);
+        TrashController.Instance.DecreaseTotalTrashAmount(storageDifference);
+        GameManager.Instance.experienceManager.experienceToIncrease += 2 * storageDifference;
+    }
+
+    void AddVehicleGarbage(float timeDifference)
+    {
+        int volunteerCount = PersistantData.Instance.sceneData.VolunteerLoaderCount;
+        int vehicleDifference = (int)(timeDifference / vehicle_timeBtwAdditions) * volunteerCount;
+        refToVehicle.AddGarbage(vehicleDifference);
     }
 }
