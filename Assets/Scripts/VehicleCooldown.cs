1dfc4dec77090e16cfe1b172d378cd0c11984110
Pijus-N <75778499+Pijus-N@users.noreply.github.com>
2023-05-23 19:02:29 +0300

    fix

--- Assets/Scripts/VehicleCooldown.cs
+++ Assets/Scripts/VehicleCooldown.cs
@@ -89,7 +89,7 @@ public class VehicleCooldown : MonoBehaviour
 
     void SendVehicle()
     {
-        SoundManager.PlaySound(SoundManager.Sound.TruckEngine);
+        SoundManager.PlaySound(SoundManager.Sound.TruckEngine, 0.1f);
         PlayParticleEffect();
         garbageFullnessAnimator.Play("Object_Dissapear");
         vehicleReturned = false;
